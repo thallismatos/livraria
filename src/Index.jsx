273e6d23116fefa689de4fb16c6ee7402d4b2e8c
Thallis Matos <thallismatos32@gmail.com>
2024-11-29 16:01:05 -0300

    chore: initialize project with basic structure

---
+++ src/Index.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header/Header';
+import Footer from "./components/footer/Footer";
+import './reset.scss';
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
