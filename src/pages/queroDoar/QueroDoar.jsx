1fd0cdfc3a2435bb6f222fda6bab76790fbb3769
Thallis Matos <thallismatos32@gmail.com>
2024-11-30 03:29:12 -0300

    feat(quero_doar): criar estrutura inicial com HTML, estilo e responsividade

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,10 +1,54 @@
-
+import Q from "./queroDoar.module.scss";
+import Book from "../../assets/livro_querodoar.svg";
 
 export default function QueroDoar() {
   return (
-    <div>
-      <h1>Quero doar</h1>
-      <p>Essa é a página de doações</p>
-    </div>
+    <section className={Q.form_container}>
+      <h2>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </h2>
+      <form className={Q.book_form}>
+        <header className={Q.form_subtitle}>
+          <img src={Book} alt="" />
+          <h3>Informações do Livro</h3>
+        </header>
+        <div className={Q.form_field}>
+          <input
+            type="text"
+            name="titulo"
+            placeholder="Título do Livro"
+            required
+          />
+        </div>
+        <div className={Q.form_field}>
+          <input
+            type="text"
+            name="categoria"
+            placeholder="Categoria do Livro"
+            required
+          />
+        </div>
+        <div className={Q.form_field}>
+          <input
+            type="text"
+            name="autor"
+            placeholder="Autor do Livro"
+            required
+          />
+        </div>
+        <div className={Q.form_field}>
+          <input
+            type="url"
+            name="linkImagem"
+            placeholder="Link da Imagem do Livro"
+            required
+          />
+        </div>
+        <button type="submit" className={Q.submit_btn}>
+          Doar
+        </button>
+      </form>
+    </section>
   );
 }
