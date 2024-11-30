f1fb827147b61aa014856b9fc8dd9e815bbcadf5
Thallis Matos <thallismatos32@gmail.com>
2024-11-30 01:20:16 -0300

    feat(header): implementar HTML, estilo, responsividade e rotas

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,6 +1,6 @@
-import Header from './components/header/Header';
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import './reset.scss';
+import "./reset.scss";
 
 export default function Index() {
   return (
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import Logo from "../../assets/logo.svg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Inicio from "../../pages/inicio/Inicio";
+import Doados from "../../pages/doados/Doados";
+import QueroDoar from "../../pages/queroDoar/QueroDoar";
+import H from "./header.module.scss";
+
+export default function Header() {
+  return (
+    <BrowserRouter>
+      <header className={H.header}>
+        <div className={H.logo}>
+          <img src={Logo} alt="" />
+        </div>
+        <nav>
+          <ul className={H.menu}>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
+            <li>
+              <Link to="livros">Livros</Link>
+            </li>
+            <li>
+              <Link to="doar">Quero doar</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={H.busca}>
+          <input type="text" placeholder="o que você precisa ?" />
+          {/* <img src={Busca} alt="" /> */}
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="doados" element={<Doados />} />
+        <Route path="doar" element={<QueroDoar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
