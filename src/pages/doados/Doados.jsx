e076c5fbf1f4ab45989dc712199359c0f368a715
Thallis Matos <thallismatos32@gmail.com>
2024-11-30 01:39:21 -0300

    chore: criar páginas 'queroDoar' e 'Doados'

---
+++ src/pages/doados/Doados.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Doados() {
+  return (
+    <div>
+      <h1>Doações</h1>
+      <p>Essa é a página de doações</p>
+    </div>
+  );
+}
---
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -0,0 +1,10 @@
+
+
+export default function QueroDoar() {
+  return (
+    <div>
+      <h1>Quero doar</h1>
+      <p>Essa é a página de doações</p>
+    </div>
+  );
+}
